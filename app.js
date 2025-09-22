8a1a13314f492729dcc088e7934b9939f592c568
ymanocha <yashmanocha19@gmail.com>
2025-09-22 22:01:23 +0530

    updated error handling and added security packages

--- app.js
+++ app.js
@@ -1,11 +1,19 @@
 require('dotenv').config();
 require('express-async-errors');
+
+// extra security packages
+const helmet = require('helmet')
+const cors = require('cors')
+const xss = require('xss-clean')
+const rateLimiter = require('express-rate-limit')
+
 const express = require('express');
 const app = express();
 
 //connectDB
 const connectDB = require('./db/connect')
 const authenticateUser = require('./middleware/authentication')
+
 //routers
 const authRouter = require('./routes/auth')
 const jobsRouter = require('./routes/jobs')
@@ -15,8 +23,17 @@ const jobsRouter = require('./routes/jobs')
 const notFoundMiddleware = require('./middleware/not-found');
 const errorHandlerMiddleware = require('./middleware/error-handler');
 
+app.set('trust proxy', 1); //whats reverse proxy??
+app.use(rateLimiter({
+  windowMs: 15*60*1000, //15 min
+  max: 100, //limit each ip to 100 req per windowms
+}))
+
 app.use(express.json());
 // extra packages
+app.use(helmet())
+app.use(cors())
+app.use(xss())
 
 // routes 
 app.use('/api/v1/auth',authRouter)
